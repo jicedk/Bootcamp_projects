02505fe5ce8af24db465b8ffa052fce1e031f97d
jirawat limawiratchaphong <125430998+jicedk@users.noreply.github.com>
2023-03-24 11:09:32 +0700

    Update ATM.py

--- python/ATM.py
+++ python/ATM.py
@@ -1,32 +1,31 @@
-class ATM:
-  def __init__(self,name,balance,ID,pin):
-      self.name = name
-      self.balance = int(balance)
-      self.ID = int(ID)
-      self.pin = int(pin)
-def change_name(self,new_name):
-    self.name = new_name
-    print(f"New name: {self.name}")
-def check_balance(self):
-    print(f"Account: {self.name}\n balance: {self.balance}")
-def check_ID(self):
-    print(f"Account: {self.name}\n ID: {self.ID}")
-def change_pin(self,new_pin):
-    self.pin = new_pin
-    print(f"Account: {self.name}\n Change New Pin success")
-def deposit(self,money):
-    self.balance += money
-    print(f"Account: {self.name} balance: {self.balance} \n success")
+class atm:
     
-scb = ATM("jirawat.L","20000","1","1234") 
-print(scb.name,scb.balance,scb.ID,scb.pin)
+    def __init__(self,name,balance,pw):
+        self.name = name
+        self.balance = balance
+        self.pw = pw
+    def change_name(self,new):
+        self.name = new
+        print(f"Account: {new}")
+    def check_balance(self):
+        print(f"Account: {self.name} \n Balance: {self.balance}")
+    def change_pw(self,pin):
+        self.pw = pin
+        print(f"Account: {self.name} \n pw: {pin}")
+    def deposit(self,money):
+        self.balance += money
+        print(f"Account: {self.name} \n Balance: {self.balance}")
+
+   
+scb = atm("jirawat.L",20000,1234) 
+print(scb.name,scb.balance,scb.pw)
 
 scb.change_name("jice")
 
 scb.check_balance()
 
-scb.deposit(50000)
+scb.deposit(1000)
 
-scb.change_pin(32649)
+scb.change_pw(32649)
 
-scb.deposit(-10000)
+scb.deposit(-20000)
